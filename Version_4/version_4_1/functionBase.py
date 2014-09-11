ebb4307bcd3b242381653e2dd00c4d1945047887
Ryan Knowles <ryanknowles12@gmail.com>
2014-09-11 21:40:41 +1000

    added load function to the functionsBase. file extension is .cdt

--- Version_4/version_4_1/functionBase.py
+++ Version_4/version_4_1/functionBase.py
@@ -1,7 +1,9 @@
 #needed modules: csv, 
 import csv
+
 #fileLocation in the format of C:/Users/Name/Desktop/cracticus_tibicen.csv
 def importCSV(fileLocation):
+    #import csv, save to dictionary
     fileDict={}
     with open(fileLocation, 'rb') as f:
         table = list(csv.reader(f))
@@ -15,5 +17,19 @@ def importCSV(fileLocation):
             fileDict[str(n)][str(item)]=table[n][h]
             h+=1
         n+=1
-    print fileDict["0"]
+    #delete table variable
+    del table
+    print fileDict[str(151)][headers[14]]
+    #save headers and dictionary to file
+    fileName = (fileLocation.split("/")[-1]).split('.')[0]
+    target = open(fileName+'.cdt', 'w')
+    target.write(str(fileDict))
 importCSV('C:/Users/Ryan/Desktop/cracticus_tibicen_112013-042014.csv')
+
+def loadCDT(fileLocation):
+    f = open(fileLocation,'r').read()
+    fileDict=eval(f)
+    print fileDict['151']['occurrenceRemarks']
+
+
+loadCDT('cracticus_tibicen_112013-042014.cdt')
