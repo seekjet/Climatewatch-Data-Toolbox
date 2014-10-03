3dbdab63af18b48337acef4d3599b737fe5ead94
Ryan Knowles <ryanknowles12@gmail.com>
2014-10-03 14:30:01 +1000

    moved GUI to Version 4, starting to integrate.

--- Version_4/version_4_1/functionBase.py
+++ Version_4/version_4_1/functionBase.py
@@ -2,8 +2,10 @@
 import csv
 
 #fileLocation in the format of C:/Users/Name/Desktop/cracticus_tibicen.csv
-def importCSV(fileLocation):
-    #import csv, save to dictionary
+def importFile(fileLocation):
+    
+
+    #import csv entries, save to dictionary, delete imported table
     fileDict={'entries':{}}
     with open(fileLocation, 'rb') as f:
         table = list(csv.reader(f))
@@ -17,13 +19,17 @@ def importCSV(fileLocation):
             fileDict['entries'][str(n)][str(item)]=table[n][h]
             h+=1
         n+=1
-    #delete table variable
     del table
+
     #save headers and dictionary to file
     fileName = (fileLocation.split("/")[-1]).split('.')[0]
     target = open(fileName+'.cdt', 'w')
     target.write(str(fileDict))
 
+    #return final dictionary
+    return fileDict
+
 def loadCDT(fileLocation):
     f = open(fileLocation,'r').read()
     fileDict=eval(f)
+    return fileDict
